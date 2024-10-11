65d26e11f60ac3fa5d57d1768bf66ed5c76806b2
OOP <truongthanhviettung@gmail.com>
2024-10-11 17:47:56 +0700

    first

---
+++ MAHOADES/DES.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace MAHOADES_NHOM9_ANTT
+{
+    partial class DES_Encrpytion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBanRo = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.radioEncryp = new System.Windows.Forms.RadioButton();
+            this.radioDecryp = new System.Windows.Forms.RadioButton();
+            this.GenKey = new System.Windows.Forms.Button();
+            this.dgvListKey = new System.Windows.Forms.DataGridView();
+            this.dgvKeyLR = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListKey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKeyLR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1407, 65);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(551, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MÃ HÓA DES";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập bản tin M:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập khóa K:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtBanRo
+            // 
+            this.txtBanRo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBanRo.Location = new System.Drawing.Point(224, 89);
+            this.txtBanRo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBanRo.Multiline = true;
+            this.txtBanRo.Name = "txtBanRo";
+            this.txtBanRo.Size = new System.Drawing.Size(332, 46);
+            this.txtBanRo.TabIndex = 3;
+            this.txtBanRo.Text = "0123456789ABCDEF";
+            this.txtBanRo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBanRo.TextChanged += new System.EventHandler(this.txtBanRo_TextChanged);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(224, 182);
+            this.txtKey.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(332, 46);
+            this.txtKey.TabIndex = 4;
+            this.txtKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtKey.TextChanged += new System.EventHandler(this.txtKey_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1, 383);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kết quả:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(224, 373);
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(332, 46);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(224, 306);
+            this.btnResult.Margin = new System.Windows.Forms.Padding(4);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(121, 41);
+            this.btnResult.TabIndex = 7;
+            this.btnResult.Text = "Nộp";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnEncrpytion_Click);
+            // 
+            // radioEncryp
+            // 
+            this.radioEncryp.AutoSize = true;
+            this.radioEncryp.Location = new System.Drawing.Point(224, 255);
+            this.radioEncryp.Margin = new System.Windows.Forms.Padding(4);
+            this.radioEncryp.Name = "radioEncryp";
+            this.radioEncryp.Size = new System.Drawing.Size(73, 20);
+            this.radioEncryp.TabIndex = 8;
+            this.radioEncryp.TabStop = true;
+            this.radioEncryp.Text = "Mã hóa";
+            this.radioEncryp.UseVisualStyleBackColor = true;
+            this.radioEncryp.CheckedChanged += new System.EventHandler(this.radioEncryp_CheckedChanged);
+            // 
+            // radioDecryp
+            // 
+            this.radioDecryp.AutoSize = true;
+            this.radioDecryp.Location = new System.Drawing.Point(405, 255);
+            this.radioDecryp.Margin = new System.Windows.Forms.Padding(4);
+            this.radioDecryp.Name = "radioDecryp";
+            this.radioDecryp.Size = new System.Drawing.Size(74, 20);
+            this.radioDecryp.TabIndex = 9;
+            this.radioDecryp.TabStop = true;
+            this.radioDecryp.Text = "Giải mã";
+            this.radioDecryp.UseVisualStyleBackColor = true;
+            this.radioDecryp.CheckedChanged += new System.EventHandler(this.radioDecryp_CheckedChanged);
+            // 
+            // GenKey
+            // 
+            this.GenKey.Location = new System.Drawing.Point(436, 306);
+            this.GenKey.Margin = new System.Windows.Forms.Padding(4);
+            this.GenKey.Name = "GenKey";
+            this.GenKey.Size = new System.Drawing.Size(121, 41);
+            this.GenKey.TabIndex = 10;
+            this.GenKey.Text = "Sinh Khóa";
+            this.GenKey.UseVisualStyleBackColor = true;
+            this.GenKey.Click += new System.EventHandler(this.GenKey_Click);
+            // 
+            // dgvListKey
+            // 
+            this.dgvListKey.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListKey.Location = new System.Drawing.Point(580, 89);
+            this.dgvListKey.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvListKey.Name = "dgvListKey";
+            this.dgvListKey.RowHeadersWidth = 51;
+            this.dgvListKey.Size = new System.Drawing.Size(345, 482);
+            this.dgvListKey.TabIndex = 11;
+            // 
+            // dgvKeyLR
+            // 
+            this.dgvKeyLR.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKeyLR.Location = new System.Drawing.Point(933, 89);
+            this.dgvKeyLR.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvKeyLR.Name = "dgvKeyLR";
+            this.dgvKeyLR.RowHeadersWidth = 51;
+            this.dgvKeyLR.Size = new System.Drawing.Size(474, 482);
+            this.dgvKeyLR.TabIndex = 12;
+            this.dgvKeyLR.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKeyLR_CellContentClick);
+            // 
+            // DES_Encrpytion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1407, 586);
+            this.Controls.Add(this.dgvKeyLR);
+            this.Controls.Add(this.dgvListKey);
+            this.Controls.Add(this.GenKey);
+            this.Controls.Add(this.radioDecryp);
+            this.Controls.Add(this.radioEncryp);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtBanRo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DES_Encrpytion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DES_Encrpytion";
+            this.Load += new System.EventHandler(this.DES_Encrpytion_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListKey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKeyLR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBanRo;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.RadioButton radioEncryp;
+        private System.Windows.Forms.RadioButton radioDecryp;
+        private System.Windows.Forms.Button GenKey;
+        private System.Windows.Forms.DataGridView dgvListKey;
+        private System.Windows.Forms.DataGridView dgvKeyLR;
+    }
+}
+
---
+++ MAHOADES/DES.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MAHOADES;
+
+
+namespace MAHOADES_NHOM9_ANTT
+{
+    public partial class DES_Encrpytion : Form
+    {
+        public DES_Encrpytion()
+        {
+            InitializeComponent();
+        }
+        public string check;
+        private void btnEncrpytion_Click(object sender, EventArgs e)
+        {
+
+            if (radioEncryp.Checked == true)
+            {
+                if (string.IsNullOrEmpty(txtBanRo.Text) == false && string.IsNullOrEmpty(txtKey.Text) == false)
+                {
+                    dgvKeyLR.Rows.Clear();
+                    dgvKeyLR.Columns.Clear();
+                    check = DesLibrary.EncrpytionDES(txtKey.Text.ToString(), txtBanRo.Text.ToString());
+                    while (check.Count() < 16)
+                    {
+                        check = 0 + check;
+                    }
+                    if (check.Count() == 16)
+                    {
+                        txtResult.Text = check;
+                    }
+                    showKeyLR();
+                }
+                else
+                    MessageBox.Show("Vui lòng nhập đầy đủ thông tin bản rõ và khóa");
+            }
+            if (radioDecryp.Checked == true)
+            {
+                if (string.IsNullOrEmpty(txtBanRo.Text) == false && string.IsNullOrEmpty(txtKey.Text) == false)
+                {
+                    dgvKeyLR.Rows.Clear();
+                    dgvKeyLR.Columns.Clear();
+                    check = DesLibrary.DecrpytionDES(txtKey.Text.ToString(), txtBanRo.Text.ToString());
+                    while (check.Count() < 16)
+                    {
+                        check = 0 + check;
+                    }
+                    if (check.Count() == 16)
+                    {
+                        txtResult.Text = check;
+                    }
+                    showKeyLR();
+                }
+                else
+                    MessageBox.Show("Vui lòng nhập đầy đủ thông tin bản rõ và khóa");
+            }
+
+        }
+
+        private void radioEncryp_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Nhập bản rõ M";
+            txtBanRo.Text = null;
+            txtBanRo.Enabled = true;
+            txtResult.Text = null;
+            btnResult.Enabled = true;
+        }
+
+        private void radioDecryp_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Nhập bản mã C";
+            txtBanRo.Text = null;
+            txtBanRo.Enabled = true;
+            txtResult.Text = null;
+            btnResult.Enabled = true;
+        }
+
+        private void DES_Encrpytion_Load(object sender, EventArgs e)
+        {
+            txtBanRo.Enabled = false;
+            txtBanRo.Text = "Chọn hình thức Mã hóa/ Giải mã";
+            btnResult.Enabled = false;
+            radioEncryp.Enabled = false;
+            radioDecryp.Enabled = false;
+        }
+        void showKey()
+        {
+            dgvListKey.Columns.Add("C1", "i");
+            dgvListKey.Columns["C1"].DataPropertyName = "Property1";
+            dgvListKey.Columns.Add("C2", "Ki");
+            dgvListKey.Columns["C2"].DataPropertyName = "Property2";
+            string[] ListKeys = DesLibrary.ListKeys(txtKey.Text);
+            int Length = ListKeys.Length;
+            for (int i = 0; i < Length; i++)
+            {
+                DataGridViewRow row = (DataGridViewRow)dgvListKey.Rows[0].Clone();
+                row.Cells[0].Value = (i + 1).ToString();
+                row.Cells[1].Value = DesLibrary.cvtBinToHex(ListKeys[i]);
+                dgvListKey.Rows.Add(row);
+            }
+        }
+        void showKeyLR()
+        {
+            dgvKeyLR.ColumnCount = 3;
+            dgvKeyLR.Columns[0].Name = "i";
+            dgvKeyLR.Columns[1].Name = "K Left";
+            dgvKeyLR.Columns[2].Name = "K right";
+
+            int Length = DesLibrary.ListKL.Length;
+            for (int i = 0; i < Length; i++)
+            {
+                dgvKeyLR.Rows.Add(i, DesLibrary.ListKL[i], DesLibrary.ListKR[i]);
+            }
+
+        }
+        private void GenKey_Click(object sender, EventArgs e)
+        {
+            showKey();
+            if (dgvListKey != null)
+            {
+                radioEncryp.Enabled = true;
+                radioDecryp.Enabled = true;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dgvKeyLR_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBanRo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtKey_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
